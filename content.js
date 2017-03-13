c1c7bd443f1432eb7bef08b441f1a3e5e127eaf5
jaspinde <jaspinde@umich.edu>
2017-03-12 23:10:12 -0400

    add background script

--- content.js
+++ content.js
@@ -1,17 +1,18 @@
-var oldLocation = location.href;
-setInterval(function() {
-if(location.href != oldLocation) {
-    title = document.getElementById("eow-title").innerHTML.split("(");
-    title1 = title[0].split("[");
-    punctuationless = title1[0].replace(/[.,\/#!|$%\^&\*;:&{}=\-_`~()]/g,"");
-    finalString = punctuationless.replace(/\s{2,}/g," ");
-    finalstring1 = finalString.split("ft")
-    finalstring2 = finalstring1[0].split("feat")
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.method == "run") {
+            $('a#spotifylink').remove();
+            
+            var title = document.getElementById("eow-title").innerHTML.split("(");
+            var title1 = title[0].split("[");
+            var title2 = title1[0].replace(/&amp;|ft.|feat/g,"");
+            var punctuationless = title2.replace(/[.,\/#!|$%\^&\*;:&{}=\-_`~()]/g,"");
+            var finalString = punctuationless.replace(/\s{2,}/g," ");
+            
+            $.get( "https://api.spotify.com/v1/search?query="+finalString+"&type=track&offset=0&limit=1", function( data ) {
+                $("#eow-title").append("<a id='spotifylink' style='color: green' href='" + data.tracks.items[0].uri+ "'><b>LISTEN ON SPOTIFY</b></a>")
+            });
 
-    $.get( "https://api.spotify.com/v1/search?query="+finalstring2[0]+"&type=track&offset=0&limit=1", function( data ) {
-        $("#eow-title").append("<a style='color: green' href='" + data.tracks.items[0].uri+ "'><b>LISTEN ON SPOTIFY</b></a>")  
-    });
-
-    oldLocation = location.href
-}
-}, 1000);
+        } 
+    }
+); 
